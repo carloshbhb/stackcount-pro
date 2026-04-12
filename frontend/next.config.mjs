a92bded187d49f1cced011a8bb4ae80e5e2566cd
carloshbhb <52144179+carloshbhb@users.noreply.github.com>
2026-04-12 14:15:41 -0300

    Create next.config.mjs

---
+++ frontend/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Isso garante que o Next.js saiba que estamos usando o diretório src
+  distDir: '.next',
+};
+
+export default nextConfig;
